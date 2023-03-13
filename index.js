ac233e791015c3aafaa783a718acd9bd4df8a891
Rashed967 <rashedhasan967.com>
2023-03-13 17:08:04 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,79 @@
+// Swap Two Variables
+
+// let a = 10;
+// let b = 20;
+// let tem = a;
+// // a = b;
+// // b = tem;
+// [b, a] = [a, b]
+// console.log(a, b)
+
+
+// 2  JavaScript Program to Convert Kilometers to Miles
+
+// const kiloToMile = kilo =>{
+//     const factor = 0.621371;
+//     const miles = kilo * factor;
+//     return miles;
+// }
+
+// const miles = kiloToMile(23);
+// console.log(miles)
+
+
+// 3. Convert Celsius to Fahrenheit
+// const celsiusToFahrenheit = celsius => {
+//     const fahrenheit = (celsius * 1.8) + 32;
+//     return fahrenheit;
+// }
+
+// const fahrenheit = celsiusToFahrenheit(30);
+// console.log(fahrenheit)
+
+
+// 4 . Check if a number is Positive, Negative, or Zero 
+
+// const checkPositiveOrNegative = number =>{
+//     if(number > 0){
+//         console.log(`${number} is positive`)
+//     }
+//     else if(number < 0){
+//         console.log(`${number} is negative`)
+//     }
+//     else{
+//         console.log(`${number} is 0`)
+//     }
+//     return number
+// }
+
+// const result = checkPositiveOrNegative(0);
+// console.log(result) 
+
+
+// 5. Check if a Number is Odd or Even
+// const checkOddEven = number =>{
+//     const result = number % 2 === 0 ? `${number} is Even Number` : `${number} is Odd Number`;
+//     return result
+// }
+
+// const result = checkOddEven(54245);
+// console.log(result)
+
+
+// 6. Find the Largest Among Three Numbers
+// const findLargestNumber = (num1, num2, num3) =>{
+//     const result = Math.max(num1, num2, num3)
+//     return result
+// }
+
+// const result = findLargestNumber(100, 350, 480);
+// console.log(result)
+
+
+7 . 
+
+
+
+
+
+
